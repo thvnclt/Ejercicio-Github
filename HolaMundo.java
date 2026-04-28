9322b554a2ed1c588dba6f53c131d2ce20dfbd8f
thvnclt <theovancelst8@gmail.com>
2026-04-28 15:44:38 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola, este es mi proyecto de GitHub");
+        System.out.println("Añadiendo info");
     }
 }
